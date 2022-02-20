9071debefdcb474341a798d0b4ed901536f37fe4
netoxneto <netoxneto@hotmail.com>
2022-02-20 12:02:14 -0300

    edit article

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -15,12 +15,67 @@ router.post("/categories/save", (req,res)=>{
             title: data,
             slug: slugify(data)
         }).then(()=>{
-            res.redirect("/");
+            res.redirect("/admin/categories");
         });
     }else{
         res.redirect('/admin/categories/new');
     }
 
+});
+
+router.get("/admin/categories",(req,res)=>{
+    category.findAll({row: true}).then((categories)=>{
+        res.render('admin/categories/index',{
+            category: categories
+        });
+    });
+});
+
+router.post("/admin/categories/delete",(req,res)=>{
+    var id = req.body.id
+
+    if(id != undefined){
+        if(!isNaN(id)){
+            category.destroy({
+                where: {id:id}
+            }).then(()=>{
+                res.redirect("/admin/categories");
+            });
+        }else{
+            res.redirect("/admin/categories");
+        }
+    }else{
+        res.redirect("/admin/categories");
+    }
+});
+
+router.get("/admin/categories/edit/:id", (req,res)=>{
+    var id = req.params.id
+
+    if(isNaN(id)){
+        res.redirect("/admin/categories");
+    }
+
+    category.findByPk(id).then((category)=>{
+        if(category != undefined){
+            res.render('admin/categories/edit',{category: category});
+        }else{
+            res.redirect("/admin/categories"); 
+        }
+    }).catch(erro => {
+        res.redirect("/admin/categories");
+    });
+});
+
+router.post("/admin/categories/update", (req,res)=>{
+    var data = req.body.upTitle
+    var id = req.body.id
+    
+    category.update({title: data, slug: slugify(data)},{
+        where:{id:id}
+    }).then(()=>{
+        res.redirect("/admin/categories");
+    });
 });
 
 module.exports = router;
