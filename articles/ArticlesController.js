0f3400d3f935aa07746804851f7832296e0006cd
netoxneto <netoxneto@hotmail.com>
2022-02-21 11:07:40 -0300

    ajustes do artigo

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,8 +1,62 @@
 const express = require('express');
 const router = express.Router();
+const categories = require('../categories/Category');
+const Articles = require('./Article');
+const slug = require('slugify');
 
-router.get("/articles", (req,res)=>{
-    res.send("Rota de artigos!");
+router.get("/admin/articles", (req,res)=>{
+    Articles.findAll({
+        include: [{model: categories}]
+    }).then((articles)=>{
+        res.render('admin/articles/index',{
+            articles: articles
+        });
+    });
+});
+
+router.get("/admin/articles/new", (req,res)=>{
+    categories.findAll().then((categories)=>{
+        res.render('admin/articles/new',{
+            categories: categories
+        });
+    });
+});
+
+router.post("/article/save", (req,res)=>{
+    var title = req.body.title
+    var article = req.body.body
+    var category =req.body.category
+
+    if(title, article, category != undefined){
+        Articles.create({
+            title: title,
+            body: article,
+            slug: slug(title),
+            categoryId: category
+        }).then(()=>{
+            res.redirect('/admin/articles');
+        });
+    }else{
+        res.redirect('/admin/articles/new');
+    }
+});
+
+router.post("/article/delete", (req,res)=>{
+    var id = req.body.id
+
+    if(id != undefined){
+        if(!isNaN(id)){
+            Articles.destroy({
+                where:{id:id}
+            }).then(()=>{
+                res.redirect('/admin/articles');
+            });
+        }else{
+            res.redirect('/admin/articles');
+        }
+    }else{
+        res.redirect('/admin/articles');
+    }
 });
 
 module.exports = router;
--- database/database.js
+++ database/database.js
@@ -2,7 +2,8 @@ const Sequelize = require('sequelize');
 
 const connection = new Sequelize('guiapress', 'root', '12345678',{
     host: '127.0.0.1',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone: '-03:00'
 });
 
 module.exports = connection;
